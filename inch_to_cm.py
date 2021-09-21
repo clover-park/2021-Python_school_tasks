561c091583e2508719d7722b6eed2c6f4cf5b0fb
clover-park <clovertoby@naver.com>
2021-09-21 11:42:45 +0900

    changed ',' into '+'

--- inch_to_cm.py
+++ inch_to_cm.py
@@ -1,10 +1,10 @@
 input_inch = float(input("inch 입력 : "))
-convert2cm = input_inch * 2.54
+convert_to_cm = input_inch * 2.54
 
 print("학번 : " + "{:20d}".format(202101318))
 print("성명 : " + "{:>20}".format('Clover'))
 
 print('=' * 30)
-print(' ' * 5 + "{:08.2f}".format(input_inch),'inch')
-print(' ' * 5 + "{:08.2f}".format(convert2cm),'cm')
+print(' ' * 5 + "{:08.2f}".format(input_inch) + ' inch')
+print(' ' * 5 + "{:08.2f}".format(convert_to_cm) + ' cm')
 print('=' * 30)
