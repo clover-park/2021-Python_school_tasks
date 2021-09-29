bbddcd6f11c51b83a5d2708da924dddc610aa8d1
clover-park <clovertoby@naver.com>
2021-09-29 16:41:57 +0900

    added "if not"
    same thing but using "if not"

--- score.py
+++ score.py
@@ -4,14 +4,24 @@ if num.isdecimal():
   score=int(num)
   if score>100:
     print('입력오류')
-  if score>=80:
+  elif score>=80:
     print('통과')
-  if score>=60:
+  elif score>=60:
     print('재시험')
-  if score<60:
+  elif score<60:
     print('과락')
 else:
   print('숫자를 입력해야합니다')
-  
-  #if not num.isdecimal():
-  #print('숫자를 입력해야 합니다')
+
+if not num.isdecimal():
+  print('숫자를 입력해야 합니다')
+else:
+  score=int(num)
+  if score>100:
+    print('입력오류')
+  elif score>=80:
+    print('통과')
+  elif score>=60:
+    print('재시험')
+  elif score<60:
+    print('과락')
