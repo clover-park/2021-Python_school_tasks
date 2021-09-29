421e38e9898f2e91015470a3e65c18f59173e712
clover-park <clovertoby@naver.com>
2021-09-29 22:00:36 +0900

    using less "if" + adding details
    with some help from my uncle
    i forgot to tell him the details...sorry

--- parking.py
+++ parking.py
@@ -1,17 +1,17 @@
 import datetime
-now= datetime.datetime.now()
+now = datetime.datetime.now()
 
-parking = int(input('차량번호:'))
+car_number = int(input('차량번호:'))
 
-if now.day % 2 ==0:
-  if parking % 2 ==0:
-    print('주차 가능')
-  elif parking % 2 ==1:
-    print('주차 불가능')
-#else:
+if now.day % 2 == 0:
+  odd_or_even_day = '짝수'
+else:
+  odd_or_even_day = '홀수'
 
-if now.day % 2 ==1:
-  if parking % 2 ==1:
-    print('주차 가능')
-  elif parking % 2 ==0:
-    print('주차 불가능')
+is_car_number_even = car_number % 2 == 0
+is_today_even = now.day % 2 == 0
+
+if is_car_number_even == is_today_even:
+  print('오늘은' + ' ' + str(now.day) + '로 ' + odd_or_even_day + '날로 주차 가능합니다.')
+else:
+  print('오늘은' + ' ' + str(now.day) + '로 ' + odd_or_even_day + '날로 주차 불가능합니다.')
