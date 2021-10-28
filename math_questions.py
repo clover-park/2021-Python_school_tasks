80a8d55de70ce05d164c16d5d2714125afc65960
clover-park <clovertoby@naver.com>
2021-10-28 14:20:08 +0900

    changed names
    tuple -> questions
    list -> question

--- math_questions.py
+++ math_questions.py
@@ -1,15 +1,14 @@
-tuple = (['3+2', 5, 3], ['6/2', 3, 5],
+questions = (['3+2', 5, 3], ['6/2', 3, 5],
 ['10-2', 8,3], ['2의 3승', 8, 4],
-['5-2*2', 1, 5]
-)
+['5-2*2', 1, 5])
 score = 0
 correct = 0
 
-for list in tuple:
-  print(list[0], ': ',end='')
+for question in questions:
+  print(question[0], ': ',end='')
   num = int(input())
-  if num == list[1]:
-    score += list[2]
+  if num == question[1]:
+    score += question[2]
     correct += 1
 
 print('정답수 : ', correct)
