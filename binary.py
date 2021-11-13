ab8d4a86fb561829983f5f5e70a70fe8ad59466e
clover-park <clovertoby@naver.com>
2021-11-13 12:39:21 +0900

    Create binary
    took like more than an hour

---
+++ binary.py
@@ -0,0 +1,22 @@
+def binary(n):
+    a, b = divmod(n, 2)
+    binary_list.append(str(b))
+    global count
+    count += 1
+    if a == 0 :
+      return binary_list
+    else :
+      return binary(a)
+
+while True:
+  num = int(input('양의 정수 입력(음수입력시 종료): '))
+  if num >= 0:
+    count = 0
+    binary_list = []
+    answer = binary(num)
+    real_binary = answer[::-1]
+    print(str(num) +'의 이진수:',''.join(real_binary), end=' ')
+    print('binary()함수 반복횟수:', count)
+    print()
+  else:
+    break
