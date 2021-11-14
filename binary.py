03d4ad72cf46b269275760357b735406281d6c19
clover-park <clovertoby@naver.com>
2021-11-14 14:48:50 +0900

    The final one
    fix indent, delete real_binary

--- binary.py
+++ binary.py
@@ -1,10 +1,10 @@
 def binary(n):
-    a, b = divmod(n, 2)
-    binary_list.append(str(b))
-    global count
-    count += 1
-    if a > 0 :
-      binary(a)
+  a, b = divmod(n, 2)
+  binary_list.append(str(b))
+  global count
+  count += 1
+  if a > 0 :
+    binary(a)
 
 while True:
   num = int(input('양의 정수 입력(음수입력시 종료): '))
@@ -12,8 +12,8 @@ while True:
     count = 0
     binary_list = []
     binary(num)
-    real_binary = binary_list[::-1]
-    print(str(num) +'의 이진수:',''.join(real_binary), end=' ')
+    binary_list[::-1]
+    print(str(num) +'의 이진수:',''.join(binary_list[::-1]), end=' ')
     print('binary()함수 반복횟수:', count)
     print()
   else:
