b49ee478efea2c0d883b401c800e552da9653a76
clover-park <clovertoby@naver.com>
2021-12-10 12:27:44 +0900

    Create score.py

--- score.py
+++ score.py
@@ -1,14 +1,38 @@
-input_number = input('정수 입력 : ')
+def total(student):
+    total_score = student[1] + student[2] + student[3]
+    return total_score
 
-if not input_number.isdecimal():
-  print('숫자를 입력해야 합니다')
-else:
-  score=int(input_number)
-  if score>100:
-    print('숫자 범위 오류!!!')
-  elif score>=80:
-    print('통과!!!')
-  elif score>=60:
-    print('재시험!!!')
-  elif score<60:
-    print('과락!!!')
+
+def ave(student):
+    average = student[4] / 3
+    return average
+
+
+def grade(student):
+    if student[5] >= 90:
+        result = 'A'
+    elif student[5] >= 80:
+        result = 'B'
+    elif student[5] >= 70:
+        result = 'C'
+    elif student[5] >= 60:
+        result = 'D'
+    else:
+        result = 'F'
+    return result
+
+
+def output(student):
+    print(student[0], ':', '국어:', student[1], '영어:', student[2], '수학:',
+          student[3], '총점:', student[4], '평균:', '{:.2f}'.format(student[5]), '학점:', student[6])
+
+
+def max_student(*student):
+    if len(student) > 0:
+        max = student[0]
+        for s in student:
+            if s[4] > max[4]:
+                max = s
+        return max
+    else:
+        return None
